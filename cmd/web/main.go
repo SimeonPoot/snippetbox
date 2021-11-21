57607bfe29b63648248f65140cfce32ed8fc908c
Simeon Poot <spoot@binck.nl>
2021-11-21 16:48:41 +0100

    refactor main, add routes.go

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -23,21 +23,19 @@ func main() {
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
 	// Initialize a new instance of application containing the dependencies.
-	app := &application{errorLog: errorLog, infoLog: infoLog}
-
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet", app.showSnippet)
-	mux.HandleFunc("/snippet/create", app.createSnippet)
-
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	app := &application{
+		errorLog: errorLog,
+		infoLog:  infoLog,
+	}
 
 	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: errorLog,
-		Handler:  mux,
+		// The routes for our application are now isolated and encapsulated in the app.routes() method,
+		// and the responsibilities of our main() function are limited to:
+		// Parsing the runtime configuration settings for the application;
+		// Establishing the dependencies for the handlers; and Running the HTTP server.
+		Handler: app.routes(), // Call the new app.routes() method,
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
